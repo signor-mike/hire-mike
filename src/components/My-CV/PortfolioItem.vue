6a1c657aa31ce4b6457a8c97a693227a46992c6b
Mike-Oxhuge <mikelitoris34@icloud.com>
2021-10-26 12:46:23 +0200

    fixes button, adds data, minor UI reworks

--- src/components/My-CV/PortfolioItem.vue
+++ src/components/My-CV/PortfolioItem.vue
@@ -1,32 +1,35 @@
 <template>
-  <v-main>
-    <h2>
-      {{ item.name }}
-    </h2>
-    <v-img width="500" :src="item.image"></v-img>
-    <h3>
-      <a :href="item.url">{{ item.url }}</a>
-    </h3>
-    <p>{{ item.description }}</p>
-    <p>{{ item.specification }}</p>
-    <v-row>
-      <v-col v-for="i in item.techs" :key="i.i">
-        <v-img :src="i" width="50"></v-img>
-      </v-col>
-    </v-row>
-    <v-divider v-if="$vuetify.breakpoint.smAndDown"></v-divider>
-  </v-main>
+	<v-main class="mx-2">
+		<h2>
+			{{ item.name }}
+		</h2>
+		<v-img width="500" height="300" :src="item.image" contain></v-img>
+		<h3>
+			<a :href="item.url">{{ item.url }}</a>
+		</h3>
+		<p>{{ item.description }}</p>
+		<p>{{ item.specification }}</p>
+		<v-row justify="space-around">
+			<v-col v-for="i in item.techs" :key="i.i" cols="auto">
+				<v-img :src="i" width="50"></v-img>
+			</v-col>
+		</v-row>
+		<v-divider v-if="$vuetify.breakpoint.smAndDown"></v-divider>
+	</v-main>
 </template>
 
 <script>
-export default {
-  props: {
-    item: {
-      type: Object,
-      required: true,
-    },
-  },
-};
+	export default {
+		props: {
+			item: {
+				type: Object,
+				required: true,
+			},
+		},
+		methods: {
+			//
+		},
+	};
 </script>
 
 <style></style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,101 +1,122 @@
 <template>
-  <v-container>
-    <div class="d-flex">
-      <v-icon color="primary" large @click="backwards()"
-        >keyboard_arrow_left</v-icon
-      >
-      <h1 class="mx-auto">My projects</h1>
-    </div>
-    <v-row justify="center" dense>
-      <v-col v-for="item in portfolio" :key="item.i" cols="12" sm="12" md="6">
-        <PortfolioItem :item="item" />
-      </v-col>
-    </v-row>
-  </v-container>
+	<v-container>
+		<div class="d-flex">
+			<v-icon color="primary" large @click="backwards()"
+				>keyboard_arrow_left</v-icon
+			>
+			<h1 class="mx-auto">My projects</h1>
+		</div>
+		<v-row justify="center" dense>
+			<v-col
+				v-for="item in portfolio"
+				:key="item.i"
+				cols="12"
+				sm="12"
+				md="6"
+				lg="4"
+				xl="3"
+			>
+				<PortfolioItem :item="item" />
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
-import PortfolioItem from "../components/My-CV/PortfolioItem.vue";
-export default {
-  components: { PortfolioItem },
-  data() {
-    return {
-      portfolio: [
-        {
-          name: "Hire Mike",
-          image: "/projects/hm.png",
-          url: "https://hire-mike.web.app",
-          description: "My very own corner in the worldwide web",
-          specification:
-            "Doing stuff here and there. This is my place and I am doing whatever I want ;)",
-          techs: [
-            "/techs/vue-js.png",
-            "/techs/vuetify.png",
-            "/techs/node.png",
-            "/techs/mongo.png",
-          ],
-        },
-        {
-          name: "F'n'S Handmade accessories",
-          image: "/projects/fns.png",
-          url: "https://fns-accessories.web.app",
-          description: "e-commerce website",
-          specification:
-            "The webstore for my wife. Still unfinished. One day we will launch it.",
-          techs: [
-            "/techs/nuxt.png",
-            "/techs/vuetify.png",
-            "/techs/node.png",
-            "/techs/mongo.png",
-          ],
-        },
-        {
-          name: "Benchmark checker",
-          image: "/projects/pain.png",
-          url: "https://strivenator.vercel.app/",
-          description: "A small app to check exam results.",
-          specification:
-            "The purpose of this app is to help fellow students of Strive school. After each benchmark students could see what exact questions they've answered wrong. But unfortunately the idea was ditched because Strive school decided to remove the 'isCorrect' property from the database. And so from now on every answer is marked as incorrect.",
-          techs: ["/techs/vue-js.png", "/techs/vuetify.png"],
-        },
-        {
-          name: "Mikes",
-          image: "/projects/mikes.png",
-          url: "https://mikes.netlify.app/",
-          description: "Just fooling around.",
-          specification:
-            "I was playing around with DOM-manipulations and a little bit of animations. Sorta playground.",
-          techs: [
-            "/techs/plain-html.png",
-            "/techs/plain-css.png",
-            "/techs/plain-js.png",
-          ],
-        },
-        {
-          name: "Guess a number",
-          image: "/projects/guesser.png",
-          url: "https://strive-m3-playground.netlify.app/",
-          description: "A very simple number guessing game.",
-          specification:
-            "If you win, your prize will amaze you. Trust me. It definetely worth it!",
-          techs: [
-            "/techs/plain-html.png",
-            "/techs/plain-css.png",
-            "/techs/plain-js.png",
-          ],
-        },
-      ],
-    };
-  },
-  mounted() {
-    console.log(this.$router.history);
-  },
-  methods: {
-    backwards() {
-      this.$router.go(-1);
-    },
-  },
-};
+	import PortfolioItem from "../components/My-CV/PortfolioItem.vue";
+	export default {
+		components: { PortfolioItem },
+		data() {
+			return {
+				portfolio: [
+					{
+						name: "Hire Mike",
+						image: "/projects/hm.png",
+						url: "https://hire-mike.web.app",
+						description: "My very own corner in the worldwide web",
+						specification:
+							"Doing stuff here and there. This is my place and I am doing whatever I want ;)",
+						techs: [
+							"/techs/vue-js.png",
+							"/techs/vuetify.png",
+							"/techs/node.png",
+							"/techs/mongo.png",
+						],
+					},
+					{
+						name: "Shopping list",
+						image: "/projects/shopass.png",
+						url: "https://shopass.vercel.app/",
+						description: "App for creating shopping list.",
+						specification:
+							"A very simple app serves a purpose of replacing pen and paper when going for shopping",
+						techs: [
+							"/techs/vue-js.png",
+							"/techs/vuetify.png",
+							"/techs/vuex.png",
+						],
+					},
+					{
+						name: "F'n'S Handmade accessories",
+						image: "/projects/fns.png",
+						url: "https://fns-accessories.web.app",
+						description: "e-commerce website",
+						specification:
+							"The webstore for my wife. Still unfinished. One day we will launch it.",
+						techs: [
+							"/techs/nuxt.png",
+							"/techs/vuetify.png",
+							"/techs/node.png",
+							"/techs/mongo.png",
+						],
+					},
+					{
+						name: "Benchmark checker",
+						image: "/projects/pain.png",
+						url: "https://strivenator.vercel.app/",
+						description: "A small app to check exam results.",
+						specification:
+							"The purpose of this app is to help fellow students of Strive school. After each benchmark students could see what exact questions they've answered wrong. But unfortunately the idea was ditched because Strive school decided to remove the 'isCorrect' property from the database. And so from now on every answer is marked as incorrect.",
+						techs: ["/techs/vue-js.png", "/techs/vuetify.png"],
+					},
+					{
+						name: "Mikes",
+						image: "/projects/mikes.png",
+						url: "https://mikes.netlify.app/",
+						description: "Just fooling around.",
+						specification:
+							"I was playing around with DOM-manipulations and a little bit of animations. Sorta playground.",
+						techs: [
+							"/techs/plain-html.png",
+							"/techs/plain-css.png",
+							"/techs/plain-js.png",
+						],
+					},
+					{
+						name: "Guess a number",
+						image: "/projects/guesser.png",
+						url: "https://strive-m3-playground.netlify.app/",
+						description: "A very simple number guessing game.",
+						specification:
+							"If you win, your prize will amaze you. Trust me. It definetely worth it!",
+						techs: [
+							"/techs/plain-html.png",
+							"/techs/plain-css.png",
+							"/techs/plain-js.png",
+						],
+					},
+				],
+			};
+		},
+		mounted() {
+			console.log(this.$router.history);
+		},
+		methods: {
+			backwards() {
+				this.$router.go(-1);
+			},
+		},
+	};
 </script>
 
 <style></style>
