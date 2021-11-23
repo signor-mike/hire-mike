93c2bc853b949710c4752fa0f6cd2bc0b44fb848
Mike-Oxhuge <mikelitoris34@icloud.com>
2021-11-23 17:35:34 +0100

    minor cleanup

--- src/main.js
+++ src/main.js
@@ -1,25 +1,30 @@
+/* CORE */
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Vuex from "vuex";
-import vuetify from "./plugins/vuetify";
 import { store } from "./store/store";
+
+/* plugins */
+import vuetify from "./plugins/vuetify";
+import "./plugins/type"; // typing animation
+
+/* 3rd party */
 import FlagIcon from "vue-flag-icon";
 import jsPDF from "jspdf";
 import html2PDF from "jspdf-html2canvas";
-import VueTypedJs from "vue-typed-js";
 
 Vue.config.productionTip = false;
 
-Vue.use(FlagIcon, VueTypedJs);
+Vue.use(FlagIcon);
 
 new Vue({
-  Vuex,
-  store,
-  router,
-  vuetify,
-  FlagIcon,
-  jsPDF,
-  html2PDF,
-  render: (h) => h(App),
+	Vuex,
+	store,
+	router,
+	vuetify,
+	FlagIcon,
+	jsPDF,
+	html2PDF,
+	render: (h) => h(App),
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,81 +1,81 @@
 <template>
-  <div class="primary--text">
-    <div class="class">
-      <vue-typed-js
-        :strings="['Hire mike']"
-        :type-speed="50"
-        :start-delay="700"
-        :show-cursor="false"
-      >
-        <h1 class="text-uppercase mb-1 typing" />
-      </vue-typed-js>
-      <br />
-      <vue-typed-js
-        :strings="['today please']"
-        :start-delay="2100"
-        :cursor-char="'_'"
-        @onComplete="draw = !draw"
-      >
-        <h3 class="typing" />
-      </vue-typed-js>
+	<div class="primary--text">
+		<div class="class">
+			<vue-typed-js
+				:strings="['Hire mike']"
+				:type-speed="50"
+				:start-delay="700"
+				:show-cursor="false"
+			>
+				<h1 class="text-uppercase mb-1 typing" />
+			</vue-typed-js>
+			<br />
+			<vue-typed-js
+				:strings="['today please']"
+				:start-delay="2100"
+				:cursor-char="'_'"
+				@onComplete="draw = !draw"
+			>
+				<h3 class="typing" />
+			</vue-typed-js>
 
-      <div v-show="draw" class="button-box">
-        <v-row dense :no-gutters="$vuetify.breakpoint.smAndDown">
-          <v-col v-for="contact in contacts" :key="contact.i" cols="auto">
-            <Contacts :contact="contact" />
-          </v-col>
-        </v-row>
-      </div>
-    </div>
-  </div>
+			<div v-show="draw" class="button-box">
+				<v-row dense :no-gutters="$vuetify.breakpoint.smAndDown">
+					<v-col
+						v-for="contact in contacts"
+						:key="contact.i"
+						cols="auto"
+					>
+						<Contacts :contact="contact" />
+					</v-col>
+				</v-row>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import Vue from "vue";
-import VueTypedJs from "vue-typed-js";
-Vue.use(VueTypedJs);
-
-import Contacts from "../components/Contacts.vue";
-export default {
-  components: { Contacts },
-  data() {
-    return {
-      draw: false,
-    };
-  },
-  mounted: function() {
-    this.wakeUp();
-  },
-  methods: {
-    async wakeUp() {
-      await fetch(`${process.env.VUE_APP_BACKEND_URL}/wakeup`);
-    },
-  },
-  computed: {
-    contacts() {
-      return this.$store.state.contacts;
-    },
-  },
-};
+	import Contacts from "../components/Contacts.vue";
+	export default {
+		components: { Contacts },
+		data() {
+			return {
+				draw: false,
+			};
+		},
+		mounted: function() {
+			this.wakeUp();
+		},
+		methods: {
+			async wakeUp() {
+				await fetch(`${process.env.VUE_APP_BACKEND_URL}/wakeup`);
+			},
+		},
+		computed: {
+			contacts() {
+				return this.$store.state.contacts;
+			},
+		},
+	};
 </script>
 
 <style scoped>
-.class {
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: calc(30% + 15px);
-  left: calc(25% - 20px);
-}
+	.class {
+		display: flex;
+		flex-direction: column;
+		position: absolute;
+		top: calc(30% + 15px);
+		left: calc(25% - 20px);
+	}
 
-.button-box {
-  position: relative;
-  left: -28px;
-  margin-top: 20%;
-}
-h1,
-h3 {
-  font-family: "Merriweather";
-}
-/* css animation / transition for drawing the div */
+	.button-box {
+		position: relative;
+		left: -28px;
+		margin-top: 20%;
+	}
+	h1,
+	h3 {
+		font-family: "Merriweather";
+	}
+	/* css animation / transition for drawing the div */
 </style>
