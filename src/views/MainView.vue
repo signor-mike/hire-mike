d1d99c272e2505dfc2b0501312e9457103fe166a
mikhail.krivoshchekov <mikhail.krivoshchekov@fifthbeat.com>
2022-09-16 22:16:55 +0200

    restores animation

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -14,27 +14,28 @@
 	import Contacts from "../blocks/Contacts.vue";
 	export default {
 		components: { About, MyWork, MyStack, Contacts },
-		// data: () => ({
-		// 	counter: 0,
-		// }),
-		// mounted: function () {
-		// 	if (this.$vuetify.theme.dark && this.$vuetify.breakpoint.mdAndUp)
-		// 		setInterval(() => {
-		// 			this.counter = this.counter + 1;
-		// 			document.getElementById(
-		// 				"gradient"
-		// 			).style.background = `conic-gradient(from ${this.counter}deg at 50% 50%, ${this.computedPallete})`;
-		// 			if (this.counter >= 360) this.counter = 0;
-		// 		}, 25);
-		// },
-		// computed: {
-		// 	computedPallete() {
-		// 		const { dark, light } = this.$vuetify.theme.themes;
-		// 		return this.$vuetify.theme.dark
-		// 			? `#ebf8e1, 10deg, ${dark.primary}, 45deg, transparent, 315deg, ${light.primary}, #ebf8e1`
-		// 			: `#ebf8e1, 10deg, ${light.primary}, 45deg, transparent, 315deg, ${dark.primary}, #ebf8e1`;
-		// 	},
-		// },
+		data: () => ({
+			counter: 0,
+		}),
+		mounted: function () {
+			this.$store.commit("SET_NAV_VISIBILITY", true);
+			if (this.$vuetify.theme.dark && this.$vuetify.breakpoint.mdAndUp)
+				setInterval(() => {
+					this.counter = this.counter + 1;
+					document.getElementById(
+						"gradient"
+					).style.background = `conic-gradient(from ${this.counter}deg at 50% 50%, ${this.computedPallete})`;
+					if (this.counter >= 360) this.counter = 0;
+				}, 25);
+		},
+		computed: {
+			computedPallete() {
+				const { dark, light } = this.$vuetify.theme.themes;
+				return this.$vuetify.theme.dark
+					? `#ebf8e1, 10deg, ${dark.primary}, 45deg, transparent, 315deg, ${light.primary}, #ebf8e1`
+					: `#ebf8e1, 10deg, ${light.primary}, 45deg, transparent, 315deg, ${dark.primary}, #ebf8e1`;
+			},
+		},
 	};
 </script>
 
