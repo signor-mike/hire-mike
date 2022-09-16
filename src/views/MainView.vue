0b85f1fca085a5dbb05ef160a9fe4f5e0a3e6fec
mikhail.krivoshchekov <mikhail.krivoshchekov@fifthbeat.com>
2022-09-16 23:48:58 +0200

    animation refinement

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -15,17 +15,20 @@
 	export default {
 		components: { About, MyWork, MyStack, Contacts },
 		data: () => ({
-			counter: 0,
+			degrees: 0,
 		}),
+		methods: {},
 		mounted: function () {
 			this.$store.commit("SET_NAV_VISIBILITY", true);
 			if (this.$vuetify.theme.dark && this.$vuetify.breakpoint.mdAndUp)
 				setInterval(() => {
-					this.counter = this.counter + 1;
+					this.degrees += 1;
 					document.getElementById(
 						"gradient"
-					).style.background = `conic-gradient(from ${this.counter}deg at 50% 50%, ${this.computedPallete})`;
-					if (this.counter >= 360) this.counter = 0;
+					).style.background = `conic-gradient(from ${this.degrees}deg
+		         at ${this.computedX}% ${this.computedY}%,
+		         ${this.computedPallete})`;
+					if (this.degrees >= 360) this.degrees = 0;
 				}, 25);
 		},
 		computed: {
@@ -34,6 +37,20 @@
 				return this.$vuetify.theme.dark
 					? `#ebf8e1, 10deg, ${dark.primary}, 45deg, transparent, 315deg, ${light.primary}, #ebf8e1`
 					: `#ebf8e1, 10deg, ${light.primary}, 45deg, transparent, 315deg, ${dark.primary}, #ebf8e1`;
+			},
+			computedX() {
+				const { page } = this.$route.query;
+				if (page === "experience") return 100;
+				if (page === "stack") return 100;
+				if (page === "contact") return 0;
+				return 0;
+			},
+			computedY() {
+				const { page } = this.$route.query;
+				if (page === "experience") return 0;
+				if (page === "stack") return 100;
+				if (page === "contact") return 100;
+				return 0;
 			},
 		},
 	};
