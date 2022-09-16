825201c360e12fc58696619993dd57702e071ac7
mikhail.krivoshchekov <mikhail.krivoshchekov@fifthbeat.com>
2022-09-16 21:49:52 +0200

    contacts page

--- src/components/CV/Contact.vue
+++ src/components/CV/Contact.vue
@@ -1,13 +1,34 @@
 <template>
 	<v-container>
-		<p>{{ contact }}</p>
+		<a :href="contact.url" target="_blank" :id="contact.title + '-image'">
+			<v-img
+				@error="handleError"
+				tag="a"
+				class="mx-auto"
+				width="100px"
+				:src="contact.image"
+				:alt="contact.title"
+			/>
+		</a>
 	</v-container>
 </template>
 
 <script>
 	export default {
 		props: {
-			contact: String,
+			contact: Object,
+		},
+		methods: {
+			handleError(e) {
+				console.log(e);
+				document.getElementById(
+					this.contact.title + "-image"
+				).innerHTML = `
+        <p class='text-center'>
+        failed to load image, click to contact via ${this.contact.title}
+        </p>
+        `;
+			},
 		},
 	};
 </script>
