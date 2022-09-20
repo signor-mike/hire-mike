1177a7bb10518f7175eec170a16e538820f40e78
mikhail.krivoshchekov <mikhail.krivoshchekov@fifthbeat.com>
2022-09-20 16:50:12 +0200

    cleanup

--- src/views/db.vue
+++ src/views/db.vue
@@ -23,37 +23,6 @@
 				:isEdit="true"
 			/>
 			<About v-else :isEdit="true" />
-			<!-- <p class="text-center" v-if="stages.currentStage === ''">
-					Please select something.
-				</p>
-				<p class="text-center primary--text" v-else>
-					Data will be automatically sorted in descending order (by
-					date)
-				</p>
-				<SkillsEditor
-					:isAdmin="isAdmin"
-					v-if="stages.currentStage === 'skills'"
-				/>
-				<ProjectEditor
-					v-if="stages.currentStage === 'projects'"
-					:isAdmin="isAdmin"
-				/>
-				<TechsEditor
-					v-if="stages.currentStage === 'techs'"
-					:isAdmin="isAdmin"
-				/>
-				<EveEditor
-					v-if="
-						stages.currentStage ===
-							'education/experience/voluntering'
-					"
-					:isAdmin="isAdmin"
-					:list="stages.stages[0]"
-				/>
-				<CvEditor
-					v-if="stages.currentStage === 'cv data'"
-					:isAdmin="isAdmin"
-				/> -->
 		</v-container>
 		<v-container v-else>
 			<h1>401 Unathorized!</h1>
@@ -62,11 +31,6 @@
 </template>
 
 <script>
-	// import SkillsEditor from "@/components/admin/SkillsEditor";
-	// import ProjectEditor from "../components/admin/ProjectEditor";
-	// import TechsEditor from "../components/admin/TechsEditor.vue";
-	// import EveEditor from "../components/admin/EveEditor.vue";
-	// import CvEditor from "../components/admin/CvEditor.vue";
 	import About from "../blocks/About.vue";
 	import MyWork from "../blocks/MyWork.vue";
 	import MyStack from "../blocks/MyStack.vue";
@@ -76,16 +40,6 @@
 	const { signOut } = authService();
 
 	export default {
-		data() {
-			return {
-				isLoading: false,
-				stages: {
-					currentStage: "",
-					stages: ["about", "my-work", "my-stack", "contacts"],
-				},
-			};
-		},
-
 		computed: {
 			isAdmin() {
 				const { user, currentUser } = this.$store.state;
@@ -108,11 +62,6 @@
 			MyWork,
 			MyStack,
 			Contacts,
-			// SkillsEditor,
-			// ProjectEditor,
-			// TechsEditor,
-			// EveEditor,
-			// CvEditor,
 		},
 	};
 </script>
