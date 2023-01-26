a5c542b19fa3ae0d3102957ffea8af921d967e40
Mike <mikelitoris34@icloud.com>
2023-01-26 20:03:14 +0100

    maintenance

--- src/plugins/fbase.js
+++ src/plugins/fbase.js
@@ -1,14 +1,23 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: process.env.VUE_APP_FIRESTORE,
-	authDomain: "hire-mike.firebaseapp.com",
-	// databaseURL: "https://hire-mike.firebaseio.com",
-	projectId: "hire-mike",
-	storageBucket: "hire-mike.appspot.com",
-	// messagingSenderId: "312088735594",
-	appId: "1:312088735594:web:3e8a41b0a41ca8c67e7f13",
-	measurementId: "G-0KLBPTLTW8",
+    // apiKey: process.env.VUE_APP_FIRESTORE,
+    // authDomain: "hire-mike.firebaseapp.com",
+    // // databaseURL: "https://hire-mike.firebaseio.com",
+    // projectId: "hire-mike",
+    // storageBucket: "hire-mike.appspot.com",
+    // // messagingSenderId: "312088735594",
+    // appId: "1:312088735594:web:3e8a41b0a41ca8c67e7f13",
+    // measurementId: "G-0KLBPTLTW8",
+    apiKey: process.env.VUE_APP_FIRESTORE,
+    authDomain: "hire-mike.firebaseapp.com",
+    databaseURL:
+        "https://hire-mike-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "hire-mike",
+    storageBucket: "hire-mike.appspot.com",
+    messagingSenderId: "312088735594",
+    appId: "1:312088735594:web:3e8a41b0a41ca8c67e7f13",
+    measurementId: "G-0KLBPTLTW8",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -21,7 +30,7 @@ export const auth = getAuth();
 
 import { store } from "@/store/store";
 auth.onAuthStateChanged((user) => {
-	store.dispatch("fetchUser", user);
+    store.dispatch("fetchUser", user);
 });
 
 import { getStorage, ref } from "firebase/storage";
@@ -29,17 +38,17 @@ export const storage = getStorage();
 export const sRef = ref(storage);
 
 const {
-	initializeAppCheck,
-	ReCaptchaV3Provider,
+    initializeAppCheck,
+    ReCaptchaV3Provider,
 } = require("firebase/app-check");
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
 export const appCheck = initializeAppCheck(app, {
-	provider: new ReCaptchaV3Provider(process.env.VUE_APP_CAPTCHA),
+    provider: new ReCaptchaV3Provider(process.env.VUE_APP_CAPTCHA),
 
-	// Optional argument. If true, the SDK automatically refreshes App Check
-	// tokens as needed.
-	isTokenAutoRefreshEnabled: true,
+    // Optional argument. If true, the SDK automatically refreshes App Check
+    // tokens as needed.
+    isTokenAutoRefreshEnabled: true,
 });
 
 // console.log("captcha:", new ReCaptchaV3Provider(process.env.VUE_APP_CAPTCHA));
