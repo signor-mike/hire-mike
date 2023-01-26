393205307580576c3df8d060c3595471f459b1f1
Mike <mikelitoris34@icloud.com>
2023-01-26 20:05:03 +0100

    cleanup

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
     transpileDependencies: ["vuetify"],
-
     pluginOptions: {},
-
-    chainWebpack: (config) => {
-        config.performance.maxEntrypointSize(400000).maxAssetSize(100000);
-    },
 };
