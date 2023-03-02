997b348ed11f44de2c16ffdd52c75642e92f0c66
Mike <mikelitoris34@icloud.com>
2023-03-02 20:15:54 +0100

    adjusts contacts

--- src/blocks/Contacts.vue
+++ src/blocks/Contacts.vue
@@ -1,51 +1,17 @@
 <template>
 	<Wrapper :title="title">
-		<v-row>
+		<v-row :dense="$vuetify.breakpoint.smAndDown">
 			<v-col
-				cols="6"
-				v-for="c in [
-					{
-						title: 'email',
-						href: 'mailto:dev_mike.k@yahoo.com',
-						icon: 'mail_outline',
-					},
-					{
-						title: 'linkedin',
-						href: 'https://linkedin.com/in/dev-mike-k',
-						icon: 'language',
-					},
-				]"
+				cols="12"
+				md="6"
+				v-for="c in $store.state.contacts.data"
 				:key="c.title"
 			>
-				<v-card>
-					<v-card-title
-						class="d-flex justify-space-between primary--text text-uppercase"
-					>
-						<a :href="c.href" target="_blank" class="text-overline">
-							{{ c.title }}
-						</a>
-						<a :href="c.href" target="_blank">
-							<v-icon color="primary">open_in_new</v-icon>
-						</a>
-					</v-card-title>
-					<v-divider />
-					<v-card-text class="d-flex">
-						<v-spacer />
-						<v-btn icon :href="c.href" target="_blank">
-							<v-icon color="secondary lighten-3">
-								{{ c.icon }}
-							</v-icon>
-						</v-btn>
-						<span class="text--secondary mx-2 my-auto">
-							{{ c.href }}
-						</span>
-						<v-spacer />
-					</v-card-text>
-				</v-card>
+				<Contact :contact="c" />
 			</v-col>
 		</v-row>
 		<v-divider class="my-5" />
-		<v-card> github </v-card>
+		<Contact :contact="$store.state.contacts.footer" />
 	</Wrapper>
 </template>
 
@@ -57,6 +23,9 @@
 				default: "Reach me out at:",
 			},
 		},
-		components: { Wrapper: () => import("@/layouts/ViewWrapper") },
+		components: {
+			Wrapper: () => import("@/layouts/ViewWrapper"),
+			Contact: () => import("@/components/Contact"),
+		},
 	};
 </script>
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -11,18 +11,44 @@
 			</a>
 		</v-card-title>
 		<v-divider />
-		<v-card-text class="d-flex">
+		<v-card-text v-if="contact.body" class="text-body-2 text-center">
+			{{ contact.body }}
+		</v-card-text>
+		<v-divider v-show="!!contact.body" />
+		<v-card-actions class="d-flex">
 			<v-spacer />
 			<v-btn icon :href="contact.href" target="_blank">
 				<v-icon color="secondary lighten-3">
 					{{ contact.icon }}
 				</v-icon>
 			</v-btn>
-			<span class="text--secondary mx-2 my-auto">
-				{{ contact.href }}
-			</span>
+			<v-btn x-small text @click="copyToClipboard">
+				{{ computedHref }}
+			</v-btn>
 			<v-spacer />
-		</v-card-text>
+		</v-card-actions>
+
+		<v-snackbar
+			v-model="snackbar"
+			top
+			timeout="1500"
+			color="primary darken-3"
+		>
+			<p class="text-center mb-0 pa-4">
+				{{ this.computedHref }} <br />
+				is copied to clipboard!
+			</p>
+			<template v-slot:action="{ attrs }">
+				<v-btn
+					color="primary lighten-3"
+					icon
+					v-bind="attrs"
+					@click="snackbar = false"
+				>
+					<v-icon>done</v-icon>
+				</v-btn>
+			</template>
+		</v-snackbar>
 	</v-card>
 </template>
 
@@ -30,6 +56,26 @@
 	export default {
 		props: {
 			contact: Object,
+		},
+		data: () => ({
+			snackbar: false,
+		}),
+		methods: {
+			copyToClipboard() {
+				navigator.clipboard.writeText(this.computedHref);
+				this.snackbar = true;
+			},
+		},
+		computed: {
+			computedHref() {
+				const toStrip = ["https://", "mailto:", "tel:", "www."];
+				let OUT = this.contact.href;
+				for (let i = 0; i < toStrip.length; i++) {
+					if (!this.contact.href.includes(toStrip[i])) continue;
+					OUT = this.contact.href.replaceAll(toStrip[i], "");
+				}
+				return OUT;
+			},
 		},
 	};
 </script>
