22233669b29557c185975015c879824adc8d1178
Mike <mikelitoris34@icloud.com>
2023-03-04 22:42:21 +0100

    adds image to about

--- src/blocks/About.vue
+++ src/blocks/About.vue
@@ -1,7 +1,12 @@
 <template>
 	<Wrapper :title="title">
-		<v-img src="https://picsum.photos/200/200?random=1"></v-img>
-		<SeeMoreButton :text="$store.state.about" :stringLength="360" />
+		<v-card class="mx-auto" max-width="344">
+			<v-img src="/mike.webp" height="200px"></v-img>
+
+			<v-card-actions>
+				<SeeMoreButton :text="$store.state.about" :stringLength="360" />
+			</v-card-actions>
+		</v-card>
 	</Wrapper>
 </template>
 
