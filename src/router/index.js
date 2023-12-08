5ea3494df46e96043d5e966a1ee2355fa8d3a078
signor-mike <mikelitoris34@icloud.com>
2023-12-08 17:21:17 +0100

    cleanup, lint

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,29 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-// import MainView from "../views/MainView.vue";
-// import NotFound from "../views/NotFound.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-	{
-		path: "/",
-		name: "Home",
-		component: Home,
-	},
-	{
-		path: "/view",
-		name: "main view",
-		component: () => import("@/views/MainView"),
-	},
-	{
-		path: "*",
-		name: "Not Found",
-		component: () => import("@/views/NotFound"),
-	},
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/view",
+        name: "main view",
+        component: () => import("@/views/MainView"),
+    },
+    {
+        path: "*",
+        name: "Not Found",
+        component: () => import("@/views/NotFound"),
+    },
 ];
 const router = new VueRouter({
-	mode: "history",
-	routes,
+    mode: "history",
+    routes,
 });
 
 export default router;
