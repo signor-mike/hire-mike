0330eb78fb31adbe4d5afaf589c472e7b9b27b20
signor-mike <mikelitoris34@icloud.com>
2023-12-09 20:27:21 +0100

    restyle in progress

--- src/blocks/MyWork.vue
+++ src/blocks/MyWork.vue
@@ -1,86 +1,15 @@
 <template>
     <Wrapper :title="title">
-        <div class="d-flex">
-            <v-btn
-                icon
-                @click="changeSlide('--')"
-                color="secondary"
-                class="my-auto"
+        <v-row justify="center" align="center">
+            <v-col
+                v-for="(prj, i) in $store.state.projects"
+                :key="prj.image"
+                :cols="i % 2 !== 0 ? '5' : '7'"
+                :md="i % 2 !== 0 ? '3' : '4'"
             >
-                <v-icon
-                    :small="$vuetify.breakpoint.xs"
-                    :x-large="$vuetify.breakpoint.mdAndUp"
-                    color="primary"
-                >
-                    {{ $vuetify.breakpoint.mdAndUp ? "chevron" : "swipe" }}_left
-                </v-icon>
-            </v-btn>
-            <v-window
-                continuous
-                v-model="model"
-                hide-delimiters
-                @change="(payload) => changeSlide(payload + 1)"
-            >
-                <v-window-item
-                    v-for="(project, i) in $store.state.projects"
-                    :key="project.id + i"
-                >
-                    <Project
-                        :project="project"
-                        :model="model"
-                        :tracker="tracker"
-                    />
-                </v-window-item>
-            </v-window>
-            <v-btn
-                icon
-                @click="changeSlide('++')"
-                color="secondary"
-                class="my-auto"
-            >
-                <v-icon
-                    :small="$vuetify.breakpoint.xs"
-                    :x-large="$vuetify.breakpoint.mdAndUp"
-                    color="primary"
-                >
-                    {{
-                        $vuetify.breakpoint.mdAndUp ? "chevron" : "swipe"
-                    }}_right
-                </v-icon>
-            </v-btn>
-        </div>
-        <v-toolbar
-            v-if="$vuetify.breakpoint.mdAndUp"
-            absolute
-            bottom
-            flat
-            dense
-            floating
-            style="left: 50%; transform: translate(-50%, 0)"
-        >
-            <template>
-                <v-btn
-                    class="mx-auto"
-                    v-for="n in $store.state.projects.length"
-                    :key="`btn-${n}`"
-                    icon
-                    @click="changeSlide(n)"
-                    :color="
-                        model === n - 1
-                            ? 'primary darken-2'
-                            : 'secondary darken-2'
-                    "
-                >
-                    <v-icon>
-                        {{
-                            model === n - 1
-                                ? "radio_button_checked"
-                                : "radio_button_unchecked"
-                        }}
-                    </v-icon>
-                </v-btn>
-            </template>
-        </v-toolbar>
+                <Project :project="prj" />
+            </v-col>
+        </v-row>
     </Wrapper>
 </template>
 
@@ -96,51 +25,12 @@
             Wrapper: () => import("@/layouts/ViewWrapper"),
             Project: () => import("@/components/Project"),
         },
-        data: () => ({
-            model: 0,
-            tracker: 0,
-        }),
-        mounted() {
-            this.tracker = this.trackingStep;
-        },
 
-        computed: {
-            trackingStep() {
-                return 100 / this.$store.state.projects.length;
-            },
-        },
+        data: () => ({}),
+        mounted() {},
 
-        methods: {
-            changeSlide(way) {
-                const length = this.$store.state.projects.length;
-                switch (way) {
-                    case "++":
-                        if (this.model + 1 === length) {
-                            this.model = 0;
-                            this.tracker = this.trackingStep;
-                        } else {
-                            this.model++;
-                            this.tracker += this.trackingStep;
-                        }
-                        break;
+        computed: {},
 
-                    case "--":
-                        if (this.model - 1 < 0) {
-                            this.model = length - 1;
-                            this.tracker = 100;
-                        } else {
-                            this.model--;
-                            this.tracker -= this.trackingStep;
-                        }
-                        break;
-
-                    default:
-                        this.model = way - 1;
-                        this.tracker = this.trackingStep * way;
-                        break;
-                }
-                return;
-            },
-        },
+        methods: {},
     };
 </script>
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,112 +1,99 @@
 <template>
-	<v-card
-		outlined
-		class="mx-auto"
-		:max-width="$vuetify.breakpoint.smAndDown ? '100%' : '60%'"
-	>
-		<v-progress-linear color="primary" :value="tracker" striped />
-		<v-card-title>
-			<a
-				:href="project.url"
-				class="text-button ma-auto text-left text-truncate"
-				target="_blank"
-			>
-				{{ project.url.replace("https://", "") }}
-			</a>
-		</v-card-title>
-		<v-card-subtitle>
-			<v-row>
-				<v-col cols="5" class="d-flex align-center justify-center">
-					<p class="text-caption mb-0 text-center">
-						{{ project.role }}
-					</p>
-				</v-col>
-
-				<v-col cols="2" class="d-flex align-center justify-center">
-					<v-divider vertical class="mx-2" />
-				</v-col>
-
-				<v-col cols="5" class="d-flex align-center justify-center">
-					<p class="text-overline mb-0 text-center">
-						{{ project.year }}
-					</p>
-				</v-col>
-			</v-row>
-		</v-card-subtitle>
-		<v-divider />
-
-		<v-card-text
-			class="secondary-lighten-5--text d-flex flex-column"
-			:class="computedTextAlign"
-		>
-			<SeeMoreButton :body="project.description" />
-		</v-card-text>
-		<v-divider />
-		<v-card-actions>
-			<v-expansion-panels
-				accordion
-				class="pa-0"
-				v-model="panel"
-				popout
-				focusable
-			>
-				<v-expansion-panel>
-					<v-expansion-panel-header>
-						<template v-slot:actions>
-							<v-icon color="primary"> expand_more </v-icon>
-						</template>
-						{{ computedActionsText }}
-					</v-expansion-panel-header>
-					<v-expansion-panel-content>
-						<v-row align="center" dense justify="space-around">
-							<v-col
-								cols="auto"
-								v-for="tech in project.techs"
-								:key="tech"
-							>
-								<span class="text-caption">
-									{{ tech }}
-								</span>
-							</v-col>
-						</v-row>
-					</v-expansion-panel-content>
-				</v-expansion-panel>
-			</v-expansion-panels>
-		</v-card-actions>
-	</v-card>
+    <v-card outlined @click="dialog = true">
+        <v-card-subtitle class="text-center">
+            {{ project.title }}
+        </v-card-subtitle>
+        <v-img width="auto" :src="project.image" class="img">
+            <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                    <v-progress-circular
+                        indeterminate
+                        color="primary"
+                    ></v-progress-circular>
+                </v-row>
+            </template>
+        </v-img>
+        <v-dialog v-model="dialog" width="1200">
+            <v-sheet>
+                <v-container>
+                    <v-row justify="center" align="center">
+                        <v-col cols="12">
+                            <v-row
+                                align="center"
+                                :class="{
+                                    'text-center':
+                                        $vuetify.breakpoint.smAndDown,
+                                }"
+                            >
+                                <v-col cols="12" md="8">
+                                    <span class="text-h6 primary--text">
+                                        {{ project.data.header }}
+                                    </span>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <span
+                                        v-for="tech in project.data.techs"
+                                        :key="tech"
+                                        class="text-overline font-weight-thin"
+                                    >
+                                        {{ tech }}
+                                    </span>
+                                </v-col>
+                            </v-row>
+                        </v-col>
+                        <v-col :cols="cols.image">
+                            <v-img
+                                :max-height="
+                                    $vuetify.breakpoint.smAndDown
+                                        ? '600'
+                                        : '1200'
+                                "
+                                contain
+                                :src="project.image"
+                            ></v-img>
+                        </v-col>
+                        <v-col :cols="cols.text">
+                            <p class="text-md-left text-center">
+                                {{ project.data.description }}
+                            </p>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-sheet>
+        </v-dialog>
+    </v-card>
 </template>
 <script>
-	export default {
-		components: {
-			SeeMoreButton: () => import("@/components/ui/SeeMoreButton"),
-		},
-		props: {
-			project: Object,
-			model: { type: Number, default: 0 },
-			tracker: { type: Number, default: 0 },
-		},
-		data: () => ({
-			panel: null,
-		}),
-		computed: {
-			computedTextAlign() {
-				switch (this.model) {
-					case 0:
-						return "text-left";
-
-					case this.$store.state.projects.length - 1:
-						return "text-right";
-
-					default:
-						return "text-center";
-				}
-			},
-			computedActionsText() {
-				if (this.panel === 0) return "Tools we used:";
-				return `${this.project.techs[0]} and ${
-					this.project.techs.length - 1
-				} more...`;
-			},
-		},
-	};
+    export default {
+        components: {},
+        props: {
+            project: Object,
+        },
+        data: () => ({
+            dialog: false,
+        }),
+        computed: {
+            cols() {
+                let OUT = {
+                    image: "6",
+                    text: "6",
+                };
+                if (this.$vuetify.breakpoint.smAndDown) {
+                    OUT.image = "12";
+                    OUT.text = "12";
+                }
+                return OUT;
+            },
+        },
+    };
 </script>
+
+<style scoped>
+    .img {
+        filter: blur(2px);
+    }
+    .img:hover,
+    .img:active {
+        filter: blur(0);
+    }
+</style>
