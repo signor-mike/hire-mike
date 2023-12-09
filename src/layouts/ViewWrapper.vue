8a7ad118dda7afa84f7e18862567524634c64add
signor-mike <mikelitoris34@icloud.com>
2023-12-09 13:54:34 +0100

    updates layout

--- src/layouts/ViewWrapper.vue
+++ src/layouts/ViewWrapper.vue
@@ -1,37 +1,38 @@
 <template>
-	<v-container fluid fill-height flex-column :class="compStyles.parent">
-		<p class="text-h5 text-center mx-auto px-2 my-3 text-capitalize">
-			{{ title }}
-		</p>
-		<v-spacer />
-		<v-container :style="compStyles.child">
-			<slot />
-		</v-container>
-		<v-spacer />
-	</v-container>
+    <v-container fluid fill-height flex-column :class="compStyles.parent">
+        <v-spacer />
+        <p class="text-h5 text-center mx-auto px-2 my-3 text-capitalize">
+            {{ title }}
+        </p>
+        <v-spacer />
+        <v-container :style="compStyles.child">
+            <slot />
+        </v-container>
+        <v-spacer />
+    </v-container>
 </template>
 
 <script>
-	export default {
-		props: { title: String },
-		computed: {
-			compStyles() {
-				if (this.$vuetify.breakpoint.smAndDown)
-					return {
-						parent: "pa-0",
-						child: "width:100%",
-					};
-				else if (this.$vuetify.breakpoint.mdAndDown)
-					return {
-						parent: "py-5 px-10",
-						child: "width:80%",
-					};
-				else
-					return {
-						parent: "py-10 px-16",
-						child: "width:50%",
-					};
-			},
-		},
-	};
+    export default {
+        props: { title: String },
+        computed: {
+            compStyles() {
+                if (this.$vuetify.breakpoint.smAndDown)
+                    return {
+                        parent: "pa-0",
+                        child: "width:100%",
+                    };
+                else if (this.$vuetify.breakpoint.mdAndDown)
+                    return {
+                        parent: "py-5 px-10",
+                        child: "width:80%",
+                    };
+                else
+                    return {
+                        parent: "py-10 px-16",
+                        child: "width:50%",
+                    };
+            },
+        },
+    };
 </script>
