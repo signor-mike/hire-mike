65dda4d8a0fbbcde7b925356f84d6f69695e850f
signor-mike <mikelitoris34@icloud.com>
2023-12-25 15:58:14 +0100

    lint

--- src/layouts/ViewWrapper.vue
+++ src/layouts/ViewWrapper.vue
@@ -1,92 +1,92 @@
 <template>
-    <v-container fill-height>
-        <v-row>
-            <v-col v-if="!!title" cols="12">
-                <p
-                    class="text-h5 text-center mx-auto px-2 my-3 text-capitalize"
-                >
-                    {{ title }}
-                </p>
-            </v-col>
-            <v-col cols="12" md="1" offset-md="1" align-self="center">
-                <v-btn
-                    :block="$vuetify.breakpoint.smAndDown"
-                    :class="{ fixed: $vuetify.breakpoint.mdAndUp }"
-                    @click="navigator('prev')"
-                    :x-large="$vuetify.breakpoint.mdAndUp"
-                    outlined
-                    color="primary"
-                >
-                    <v-icon>keyboard_double_arrow_left</v-icon>
-                </v-btn>
-            </v-col>
-            <v-col cols="12" md="8">
-                <slot />
-            </v-col>
-            <v-col cols="12" md="1" offset-md="-1" align-self="center">
-                <v-btn
-                    :block="$vuetify.breakpoint.smAndDown"
-                    :class="{ fixed: $vuetify.breakpoint.mdAndUp }"
-                    @click="navigator('next')"
-                    :x-large="$vuetify.breakpoint.mdAndUp"
-                    outlined
-                    color="primary"
-                >
-                    <v-icon>keyboard_double_arrow_right</v-icon>
-                </v-btn>
-            </v-col>
-        </v-row>
-    </v-container>
+	<v-container fill-height>
+		<v-row>
+			<v-col v-if="!!title" cols="12">
+				<p
+					class="text-h5 text-center mx-auto px-2 my-3 text-capitalize"
+				>
+					{{ title }}
+				</p>
+			</v-col>
+			<v-col cols="12" md="1" offset-md="1" align-self="center">
+				<v-btn
+					:block="$vuetify.breakpoint.smAndDown"
+					:class="{ fixed: $vuetify.breakpoint.mdAndUp }"
+					@click="navigator('prev')"
+					:x-large="$vuetify.breakpoint.mdAndUp"
+					outlined
+					color="primary"
+				>
+					<v-icon>keyboard_double_arrow_left</v-icon>
+				</v-btn>
+			</v-col>
+			<v-col cols="12" md="8">
+				<slot />
+			</v-col>
+			<v-col cols="12" md="1" offset-md="-1" align-self="center">
+				<v-btn
+					:block="$vuetify.breakpoint.smAndDown"
+					:class="{ fixed: $vuetify.breakpoint.mdAndUp }"
+					@click="navigator('next')"
+					:x-large="$vuetify.breakpoint.mdAndUp"
+					outlined
+					color="primary"
+				>
+					<v-icon>keyboard_double_arrow_right</v-icon>
+				</v-btn>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <style>
-    .fixed {
-        position: fixed;
-        top: 50%;
-        transform: translate(0, 50%);
-    }
+.fixed {
+	position: fixed;
+	top: 50%;
+	transform: translate(0, 50%);
+}
 </style>
 
 <script>
-    export default {
-        props: {
-            title: { type: String, default: "" },
-        },
-        methods: {
-            navigator(param) {
-                if (!param) {
-                    console.error("Missing param.");
-                    return;
-                }
-                if (!this.$route.query.page || !this.$store.state.navLinks) {
-                    console.error(
-                        "Missing $route.query.page or $store.state.navLinks"
-                    );
-                    return;
-                }
+export default {
+	props: {
+		title: { type: String, default: "" },
+	},
+	methods: {
+		navigator(param) {
+			if (!param) {
+				console.error("Missing param.");
+				return;
+			}
+			if (!this.$route.query.page || !this.$store.state.navLinks) {
+				console.error(
+					"Missing $route.query.page or $store.state.navLinks"
+				);
+				return;
+			}
 
-                const { page } = this.$route.query;
-                const links = this.$store.state.navLinks;
-                const go = (to) => this.$router.push(`?page=${to}`);
+			const { page } = this.$route.query;
+			const links = this.$store.state.navLinks;
+			const go = (to) => this.$router.push(`?page=${to}`);
 
-                links.forEach((l, i) => {
-                    if (l.query !== page) return;
-                    switch (param) {
-                        case "next":
-                            if (i === links.length - 1) go(links[0].query);
-                            else go(links[i + 1].query);
-                            return;
+			links.forEach((l, i) => {
+				if (l.query !== page) return;
+				switch (param) {
+					case "next":
+						if (i === links.length - 1) go(links[0].query);
+						else go(links[i + 1].query);
+						return;
 
-                        case "prev":
-                            if (i === 0) go(links[links.length - 1].query);
-                            else go(links[i - 1].query);
-                            return;
-                        default:
-                            console.error("Unrecognized direction: ", param);
-                            break;
-                    }
-                });
-            },
-        },
-    };
+					case "prev":
+						if (i === 0) go(links[links.length - 1].query);
+						else go(links[i - 1].query);
+						return;
+					default:
+						console.error("Unrecognized direction: ", param);
+						break;
+				}
+			});
+		},
+	},
+};
 </script>
