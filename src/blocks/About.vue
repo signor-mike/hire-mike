aef1473816f4b4de36cc526d1d1b047e8eb8f731
signor-mike <mikelitoris34@icloud.com>
2023-12-25 19:53:16 +0100

    tries new layout

--- src/blocks/About.vue
+++ src/blocks/About.vue
@@ -1,19 +1,33 @@
 <template>
-    <Wrapper :title="$store.state['about-title']">
-        <v-card class="mx-auto" max-width="344">
-            <v-img src="/mike.webp" height="200px"></v-img>
-            <v-card-actions>
-                <SeeMoreButton :body="$store.state.about" />
-            </v-card-actions>
-        </v-card>
-    </Wrapper>
+	<Wrapper
+		:title="$vuetify.breakpoint.mdAndUp ? $store.state['about-title'] : ''"
+	>
+		<Grid>
+			<template #top-large>
+				<v-sheet width="100%" height="100%"></v-sheet>
+			</template>
+			<template #top-small>
+				<v-img src="/mike.webp" height="200px"></v-img>
+			</template>
+			<template #bottom-small>
+				<SeeMoreButton
+					:body="$store.state.about"
+					:bodyLength="$vuetify.breakpoint.smAndDown ? 60 : 240"
+				/>
+			</template>
+			<template #bottom-large>
+				<v-sheet width="100%" height="100%"></v-sheet>
+			</template>
+		</Grid>
+	</Wrapper>
 </template>
 
 <script>
-    export default {
-        components: {
-            Wrapper: () => import("@/layouts/ViewWrapper"),
-            SeeMoreButton: () => import("@/components/ui/SeeMoreButton"),
-        },
-    };
+export default {
+	components: {
+		Wrapper: () => import("@/layouts/ViewWrapper"),
+		Grid: () => import("@/layouts/GridWrapper"),
+		SeeMoreButton: () => import("@/components/ui/SeeMoreButton"),
+	},
+};
 </script>
