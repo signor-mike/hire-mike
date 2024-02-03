f6d7b9411e40c7b39254b946fc98af540d53b4e4
signor-mike <mikelitoris34@icloud.com>
2024-02-03 18:05:06 +0100

    restores autopick of theme

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,21 @@
 </template>
 
 <script>
-	// TODO: remember user choice of theme
-	export default {
-		name: "App",
+// TODO: remember user choice of theme
+export default {
+	name: "App",
 
-		components: {
-			NavBar: () => import("@/blocks/Navbar"),
-			ControlsBar: () => import("@/blocks/ControlsBar"),
-		},
-		beforeMount: function () {
-			// if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+	components: {
+		NavBar: () => import("@/blocks/Navbar"),
+		ControlsBar: () => import("@/blocks/ControlsBar"),
+	},
+	beforeMount: function () {
+		if (window.matchMedia("(prefers-color-scheme: dark)").matches)
 			this.$vuetify.theme.dark = true;
-			// else this.$vuetify.theme.dark = false;
-		},
-	};
+		else this.$vuetify.theme.dark = false;
+	},
+};
 </script>
 <style>
-	@import url("/public/global");
+@import url("/public/global");
 </style>
