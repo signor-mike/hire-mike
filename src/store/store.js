75188e6d37fbc55d7fd84479c28ab729a068776c
signor-mike <mikelitoris34@icloud.com>
2024-06-22 16:08:01 +0200

    updates texts

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,8 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
 	state: {
 		gdpr: "According to the General Data Protection Regulation (GDPR) (EU) 2016/679. Information contained on this website may be privileged or confidential and intended for the exclusive use of the original recipient. If you have visited this website by mistake, please leave the website immediately and delete it from your history. Any information contained on this website are confidential and are intended solely for the use of the individual or entity to whom they are addressed. If you visited this website in error, you must not copy, distribute or disclose of the information it contains.",
-		appInfo: "foobar",
+		appInfo:
+			"Skilled frontend developer with expertise in JavaScript, TypeScript, and modern reactive libraries like React.js and Vue.js. Experience building mobile applications with React Native and Ionic. Utilizes Tailwind CSS, plain CSS (including preprocessors), and component libraries to optimize development. Backend proficiency in Node.js and Express.js. Maintains version control with Git (Github/Gitlab) and implements automated testing with Cypress, Detox, and Vitest. Works effectively on Ubuntu Linux, leveraging GCP and Firebase for cloud-based solutions.",
 		isNavVisible: false,
 	},
 	mutations: {
